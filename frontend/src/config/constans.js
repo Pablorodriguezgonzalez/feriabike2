548b1137ee1b8d04133ac0bde198978d2e2604fd
Pablo Rodriguez <pa.rodriguezg@gmail.com>
2025-06-24 09:56:26 -0400

    version 1.3

--- frontend/src/config/constans.js
+++ frontend/src/config/constans.js
@@ -1,5 +1,6 @@
 //export const URLBASE = "http://localhost:3000";
-export const URLBASE = "https://feriabike-back.onrender.com";
+//export const URLBASE = "https://feriabike-back.onrender.com";
+export const URLBASE = "https://feriabike2.onrender.com";
 
 export const ENDPOINT = {
   login: `${URLBASE}/login`,
